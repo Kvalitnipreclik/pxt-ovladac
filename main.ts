020e90ab0d402de594d44627918bec988e036e72
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-05-10 13:32:20 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
-
 // promene na hybani
-let interval = 20
+let interval = 500
 
 //promene na radio
-radio.setGroup(1)
+radio.setGroup(93)
 radio.setTransmitPower(7)
-radio.setFrequencyBand(0)
+radio.setFrequencyBand(5)
 
 //promene
 let xmod = 0
@@ -16,23 +15,33 @@ input.onButtonPressed(Button.A, function () {
 
     if (zamek === true) {
         zamek = false
-       
+        basic.showNumber(0)
+
     } else {
         zamek = true
-        
+        basic.showNumber(1)
+
     }
 
-   
+
+
+
+})
+
+input.onButtonPressed(Button.B, function () {
+basic.showNumber(5)
+radio.sendString("nigger")
+
 
 
 })
 
 
-function pack(x: number) {
+function pack(xmod: number) {
 
 
     xmod = input.acceleration(Dimension.X) + 1024
-    xmod = Math.floor(xmod / 8)
+    xmod = Math.floor(xmod / 16)
     posilaciString = posilaciString + String.fromCharCode(x)
 
 }
@@ -44,16 +53,15 @@ basic.forever(function () {
     if (zamek) {
 
 
-        x = input.acceleration(Dimension.X)
-        y = input.acceleration(Dimension.Y)
+     
         pack(x)
         pack(y)
         console.log(posilaciString)
+        basic.showString(posilaciString)
 
-      
         radio.sendString(posilaciString)
         posilaciString = ""
-        
+
         basic.pause(interval)
     }
 })
